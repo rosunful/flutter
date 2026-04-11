2f0cff62f05d600f27f8ca371204f8bdfb7a081e
Dura_Hora <durainbachelor100@gmail.com>
2026-04-12 00:09:18 +0545

    day-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Fura());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Fura extends StatelessWidget {
+  const Fura({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:AppBar(
-          backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Title(color: Colors.green, child: Text("Lovely")),
+          backgroundColor: Colors.lightBlue,
         ),
-        body: Text("Dura"),
-      )
+        body: Column(
+          children: [
+            Text("data"),
+            TextButton(
+              onPressed: () {
+                print("object");
+              },
+              child: Text("Btn 1"),
+            ),
+            Text("Name"),
+            ElevatedButton(
+              onPressed: () {
+                print("Button has clicked");
+              },
+              child: Text("Btn 2"),
+            ),
+            Text("Submit"),
+            OutlinedButton(
+              onPressed: () {
+                print("object");
+              },
+              child: Text("Btn 3"),
+            ),
+            Container(height: 100, width: 100, child: Center(child: Image.asset("assets/images/boy.png"))),
+          ],
+        ),
+      ),
     );
   }
 }
